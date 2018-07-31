8285231982124c5c8281664ed30342015263f59b
Azmarie <azmarie.yx@gmail.com>
2018-07-30 22:47:55 -0700

    [Day 1] Added babie component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BabieComponent } from './babie/babie.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BabieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
