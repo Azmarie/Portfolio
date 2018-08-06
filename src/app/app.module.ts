706503c2d8522799cceb45de98ce95887b0a07a4
Azmarie <azmarie.yx@gmail.com>
2018-08-06 13:20:28 -0700

    [Day2/app] rm angular-material import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { BabieComponent } from './babie/babie.component';
-
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { ProjectsComponent } from './projects/projects.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BabieComponent
+    BabieComponent,
+    ProjectsComponent,
+    GalleryComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
